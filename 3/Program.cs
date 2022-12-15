187f8e4c6a13d6127bb1ad1a895834cc5697bcd5
WIN-H1HBIIFATSD <kvetkinevgenij@gmail.com>
2022-12-15 19:53:29 +0300

    Programm 3

---
+++ 3/Program.cs
@@ -0,0 +1,23 @@
+﻿//заполнение массива случайными двузначными числами
+            Random rand = new Random();
+            double[] numbers = new double[4];
+            double result = 0;
+            for (int x = 0; x < numbers.Length; x++)
+            {
+                numbers[x] = rand.Next(10, 100);
+                Console.Write(numbers[x] + " ");
+
+            }
+            //нахождение разницы между максимальным и минимальным элементов массива.
+
+            foreach (double number in numbers)
+            {
+                result = numbers.Max() - numbers.Min();
+                
+            }
+
+
+
+            //вывод разницы между максимальным и минимальным элементов массива.
+            Console.WriteLine($"Разница между максимальным и минимальным элементов массива: {result}");
+            Console.ReadLine();
