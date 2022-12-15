409dd9994f0a80bc9e8edb6b5b86cafcbb81c1f2
WIN-H1HBIIFATSD <kvetkinevgenij@gmail.com>
2022-12-15 19:48:13 +0300

    Programm 1

---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿//заполнение массива случайными трехзначными числами
+            Random rand = new Random();
+            int[] numbers = new int[4];
+            
+            for (int x = 0; x < numbers.Length; x++)
+            {
+                numbers[x] = rand.Next(100 , 1000);
+                Console.Write(numbers[x] + " ");
+            }
+            //нахождение количества четных элементов
+            int result = 0;
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0 && number != 0)
+                {
+                    result++;
+                }
+            }
+
+            //вывод кол-ва четных элементов
+            Console.WriteLine($"Количество четных элементов в массиве: {result}");
+            Console.ReadLine();
